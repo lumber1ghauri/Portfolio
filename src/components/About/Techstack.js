efbf553385af3718c8d8dfd39eff866146819e39
lumber1ghauri <ukashazahid3@gmail.com>
2025-03-20 19:51:49 +0500

    multiple codes removed

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -11,11 +11,7 @@ import {
   DiJava,
 } from "react-icons/di";
 import {
-  SiRedis,
-  SiFirebase,
   SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
 } from "react-icons/si";
 import { TbBrandGolang } from "react-icons/tb";
 
